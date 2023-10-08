3374df87dd7b97d4fb6ac726466f8ef0b8ded6c4
Trusha Shah <143470875+trushashah14@users.noreply.github.com>
2023-10-08 18:43:32 +0530

    Add files via upload

---
+++ src/Components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => {
+    return (
+        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img alt="robots" width={220} height={200} src={ `https://robohash.org/${id}`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
